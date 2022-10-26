f04e0e16880d09c7cf12603a75344167e9236123
Kirigas <100741364+Kirigas@users.noreply.github.com>
2022-10-26 00:05:43 -0300

    Initial commit

---
+++ Desafios JS/my_script.js
@@ -0,0 +1,7 @@
+
+ 
+document.getElementById('botao').addEventListener("click", function(){
+ var nomecompleto = document.getElementById('botao') = getElementById('name').value
+    alert('Seu nome é: '+ nomecompleto);
+
+})
